600d113b2cb3ae1035fc1abd78860a13172b12be
shubhamkv <shubh082002@gmail.com>
2023-07-25 16:28:09 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,47 +1,44 @@
-import React from 'react'
-import { GiCompass, GiDiamondHard, GiStabbedNote } from 'react-icons/gi'
+import React from "react";
+import { GiCompass, GiDiamondHard, GiStabbedNote } from "react-icons/gi";
 export const links = [
   {
     id: 1,
-    text: 'home',
-    url: '/',
+    text: "home",
+    url: "/",
   },
   {
     id: 2,
-    text: 'about',
-    url: '/about',
+    text: "about",
+    url: "/about",
   },
   {
     id: 3,
-    text: 'products',
-    url: '/products',
+    text: "products",
+    url: "/products",
   },
-]
+];
 
 export const services = [
   {
     id: 1,
     icon: <GiCompass />,
-    title: 'mission',
-    text:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+    title: "mission",
+    text: " Our mission is to revolutionize the way people furnish their homes, making the process of finding the perfect furniture an enjoyable and inspiring experience.",
   },
   {
     id: 2,
     icon: <GiDiamondHard />,
-    title: 'vision',
-    text:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+    title: "vision",
+    text: "Our vision is to be the leading destination for furniture and interior design inspiration, where every customer can effortlessly discover, create, and curate their dream living spaces.",
   },
   {
     id: 3,
     icon: <GiStabbedNote />,
-    title: 'history',
-    text:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+    title: "history",
+    text: "Founded in 2020, Comfy Sloth was born out of the desire to create a seamless and inspiring online platform that would cater to the diverse needs and preferences of furniture enthusiasts.",
   },
-]
+];
 
-export const products_url = 'https://course-api.com/react-store-products'
+export const products_url = "https://course-api.com/react-store-products";
 
-export const single_product_url = `https://course-api.com/react-store-single-product?id=`
+export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
