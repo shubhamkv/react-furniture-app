0325fbce7cf19f6f6ca20052698c489b96e9c0b6
shubhamkv <shubh082002@gmail.com>
2023-07-25 16:32:58 +0530

    Update StripeCheckout.js

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -126,7 +126,10 @@ const CheckoutForm = () => {
         {/* Show a success message upon completion */}
         <p className={succeeded ? "result-message" : "result-message hidden"}>
           Payment succeeded, see the result in your
-          <a href={`https://dashboard.stripe.com/test/payments`}>
+          <a
+            href={`https://dashboard.stripe.com/test/payments`}
+            target="_blank"
+          >
             {" "}
             Stripe dashboard.
           </a>{" "}
