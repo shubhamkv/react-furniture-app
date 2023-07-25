67fe31ceea88cd08fae22cb9828fca0a2b7e9e40
shubhamkv <shubh082002@gmail.com>
2023-07-25 16:28:05 +0530

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,35 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 const AboutPage = () => {
   return (
     <main>
-      <PageHero title='about' />
-      <Wrapper className='page section section-center'>
-        <img src={aboutImg} alt='nice desk' />
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
         <article>
-          <div className='title'>
+          <div className="title">
             <h2>our story</h2>
-            <div className='underline'></div>
+            <div className="underline"></div>
           </div>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
-            accusantium sapiente tempora sed dolore esse deserunt eaque
-            excepturi, delectus error accusamus vel eligendi, omnis beatae.
-            Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
-            dolore, obcaecati incidunt sequi blanditiis est exercitationem
-            molestiae delectus saepe odio eligendi modi porro eaque in libero
-            minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
-            ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
-            similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
-            iste.
+            Welcome to Comfy Sloth - Your Ultimate Destination for Furniture and
+            Interior Design!
+          </p>
+          <p>
+            At Comfy Sloth, we are more than just a furniture store; we are your
+            trusted partner in creating beautiful and inviting living spaces.
+            Our passion for furniture and design drives us to curate an
+            exceptional collection of furniture pieces that cater to a variety
+            of tastes and preferences. Our mission is to revolutionize the way
+            people furnish their homes, making the process enjoyable, inspiring,
+            and convenient.
           </p>
         </article>
       </Wrapper>
     </main>
-  )
-}
+  );
+};
 const Wrapper = styled.section`
   display: grid;
   gap: 4rem;
@@ -56,5 +57,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
