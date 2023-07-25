831ce02e21a6113a5bc895454cbe1b8d26c0bcfa
shubhamkv <shubh082002@gmail.com>
2023-07-25 16:27:57 +0530

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
-import { services } from '../utils/constants'
+import React from "react";
+import styled from "styled-components";
+import { services } from "../utils/constants";
 const Services = () => {
   return (
     <Wrapper>
-      <div className='section-center'>
-        <article className='header'>
+      <div className="section-center">
+        <article className="header">
           <h3>
             custom furniture <br /> built only for you
           </h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-            dolorum debitis consectetur reprehenderit non aliquam voluptates
-            dolore aut vero consequuntur.
+            Welcome to the world of bespoke furniture, where your imagination
+            meets craftsmanship! We proudly presents our Custom Furniture
+            facility, where you can bring your dream designs to life.
           </p>
         </article>
-        <div className='services-center'>
+        <div className="services-center">
           {services.map((service) => {
-            const { id, icon, title, text } = service
+            const { id, icon, title, text } = service;
             return (
-              <article className='service' key={id}>
-                <span className='icon'>{icon}</span>
+              <article className="service" key={id}>
+                <span className="icon">{icon}</span>
                 <h4>{title}</h4>
                 <p>{text}</p>
               </article>
-            )
+            );
           })}
         </div>
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.section`
   h3,
@@ -94,5 +94,5 @@ const Wrapper = styled.section`
       transform: translateY(5rem);
     }
   }
-`
-export default Services
+`;
+export default Services;
